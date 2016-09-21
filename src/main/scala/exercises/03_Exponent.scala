53228c7d2ddc648351c20095d1d075b947390bfa
Ben Wiedermann <benw@cs.hmc.edu>
2016-09-21 13:01:59 -0700

    Working on expt

--- src/main/scala/exercises/03_Exponent.scala
+++ src/main/scala/exercises/03_Exponent.scala
@@ -12,6 +12,14 @@ object Exponent {
    * NOTE: Scala has a built-in exponent function, but you're not allowed to
    * use it to implement `expt`
    */
-  def expt(n: Int, e: Int): Int = -1
+  def expt(n: Double, e: Int): Double =
+      if (e == 0)
+        1
+      else if (n == 0)
+        0 
+      else if (e < 0)
+        1 / expt(n, e)      
+      else
+        n * expt(n, e-1)
 
 }
