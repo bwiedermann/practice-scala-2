e75203512146465664c203f8bbd053b11f4d7446
Ben Wiedermann <benw@cs.hmc.edu>
2016-09-21 14:19:04 -0700

    palindrome

--- src/main/scala/exercises/07_Palindrome.scala
+++ src/main/scala/exercises/07_Palindrome.scala
@@ -8,6 +8,10 @@ object Palindrome {
    *  Hint: the Scala collections API is your friend
    *  http://docs.scala-lang.org/overviews/collections/overview.html 
    */
-  def isPalindrome(s: String): Boolean = throw new NotImplementedError
+  def isPalindrome(s: String): Boolean = {
+    // remove punctuation and convert every letter to lowercase
+    val data = s.filter(_.isLetter).map(_.toLower)
+    data == data.reverse
+  }
 
 }
